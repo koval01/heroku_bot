7a5c480658d9af04ad074c96f717e1c0b7332ee3
yaroslav.koval <koval.developer@gmail.com>
2021-08-09 18:35:29 +0300

    update

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from dispatcher import dp
-from utils import porfirevich
+from utils import porfirevich, telegraph_create, create_inline_buttons
 from dispatcher import bot
 
 
@@ -8,4 +8,11 @@ from dispatcher import bot
 async def echo_message(msg: types.Message):
     await bot.send_chat_action(msg.from_user.id, 'typing')
     add_ = await porfirevich(msg.text)
-    await msg.reply("<i>%s</i><b>%s</b>" % (msg.text, add_))
+
+    text_ = "<i>%s</i><b>%s</b>" % (msg.text, add_)
+    telegraph_ = telegraph_create(text_)
+    link = create_inline_buttons(
+        {"text": "Telegra.ph", "url": telegraph_}
+    )
+
+    await msg.reply(text_, reply_markup=link)
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 from random import randint
+from telegraph import Telegraph
 from json import loads
-import aiohttp, logging
+import aiohttp, logging, re
 
 
-async def porfirevich(user_text):
+async def porfirevich(user_text: str) -> str:
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15"
     }
@@ -20,3 +21,40 @@ async def porfirevich(user_text):
                 return loads(await resp.text())["replies"][0]
     except aiohttp.ClientError as e:
         logging.error("%s: %s" % (porfirevich.__name__, e))
+
+
+def striphtml(data: str) -> str:
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+
+async def telegraph_create(text: str) -> str:
+    telegraph = Telegraph()
+    telegraph.create_account(short_name='dark_fantasy0_bot')
+
+    title_text = striphtml(text)
+
+    if len(title_text) > 16:
+        title_text = title_text[:13] + "..."
+
+    response = telegraph.create_page(
+        title_text,
+        html_content=text,
+        author_name="@dark_fantasy0_bot",
+        author_url="https://t.me/dark_fantasy0_bot",
+    )
+    return 'https://telegra.ph/' + response['path']
+
+
+def create_inline_buttons(*button) -> str:
+    array_button = []
+    global_array_buttons = []
+
+    for i in button:
+        array_button = []
+        array_button.append(i)
+        global_array_buttons.append(array_button)
+
+    result = dict(inline_keyboard=global_array_buttons)
+
+    return result
