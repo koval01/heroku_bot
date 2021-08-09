1534dc7740e5b0762ec09da0e3da07fa4def1911
yaroslav.koval <koval.developer@gmail.com>
2021-08-09 17:41:35 +0300

    update

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,9 +1,11 @@
 from aiogram import types
 from dispatcher import dp
 from ..utils import porfirevich
+from ..dispatcher import bot
 
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
+    await bot.send_chat_action(msg.from_user.id, 'typing')
     add_ = await porfirevich(msg.text)
     await msg.reply("<i>%s</i><b>%s</b>" % (msg.text, add_))
