3a02d079cb0aca2ee80a47e327d353358f7729a2
yaroslav.koval <koval.developer@gmail.com>
2021-08-09 18:46:13 +0300

    update

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -4,6 +4,20 @@ from utils import porfirevich, telegraph_create, create_inline_buttons
 from dispatcher import bot
 
 
+@dp.message_handler(commands="start")
+async def cmd_ping_bot(msg: types.Message):
+    await bot.send_chat_action(msg.from_user.id, 'typing')
+    add_ = await porfirevich("Феминизм это")
+
+    text_ = "<i>%s</i><b>%s</b>" % ("Феминизм это", add_)
+    telegraph_ = await telegraph_create(text_)
+    link = await create_inline_buttons(
+        {"text": "Telegra.ph", "url": telegraph_}
+    )
+
+    await msg.reply(text_, reply_markup=link)
+
+
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_chat_action(msg.from_user.id, 'typing')
