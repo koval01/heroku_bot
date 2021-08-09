8e5f42f141b03ef9b5c9a112ae84b424c01716fd
yaroslav.koval <koval.developer@gmail.com>
2021-08-09 18:07:30 +0300

    update

--- utils.py
+++ utils.py
@@ -1,13 +1,16 @@
 from random import randint
 from json import loads
-import aiohttp
+import aiohttp, logging
 
 
 async def porfirevich(user_text):
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://pelevin.gpt.dobro.ai/generate/", json={
-            "prompt": user_text,
-            "length": randint(10, 60),
-            "num_samples": 1,
-        }) as response:
-            return loads(await response.text())["replies"][0]
+    headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15"}
+    json = {"prompt": user_text, "length": randint(10, 60), "num_samples": 1}
+    url = "https://pelevin.gpt.dobro.ai/generate/"
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers, json=json) as resp:
+                return loads(await resp.text())["replies"][0]
+    except aiohttp.ClientError as e:
+        logging.error("%s: %s" % (porfirevich.__name__, e))
