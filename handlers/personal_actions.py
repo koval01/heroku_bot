84bb7b11a18aa7dd3b41d0a3eb24637a4f50ac82
yaroslav.koval <koval.developer@gmail.com>
2021-08-17 13:00:16 +0300

    update

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,27 +1,40 @@
+import logging, asyncio
+
 from aiogram import types
 from dispatcher import dp
 from utils import porfirevich, telegraph_create, create_inline_buttons
+from telegraph.exceptions import TelegraphException
 from dispatcher import bot
 
 
 async def send_(msg: object) -> None:
-    try:
-        await bot.send_chat_action(msg.from_user.id, 'typing')
-        add_ = await porfirevich(msg.text)
-        data_ = add_["json_"]
-        
-        for i in data_:
-            text_ = "<i>%s</i><b>%s</b>" % (msg.text, i)
-            telegraph_ = await telegraph_create(text_)
+    while True:
+        try:
+            await bot.send_chat_action(msg.from_user.id, 'typing')
+            add_ = await porfirevich(msg.text)
+            data_ = add_["json_"]
 
-            link = await create_inline_buttons(
-                {"text": "Telegra.ph", "url": telegraph_}
+            for i in data_:
+                text_ = "<i>%s</i><b>%s</b>" % (msg.text, i)
+                telegraph_ = await telegraph_create(text_)
+
+                link = await create_inline_buttons(
+                    {"text": "Telegra.ph", "url": telegraph_}
+                )
+
+                await msg.reply(text_, reply_markup=link)
+
+        except TelegraphException as e:
+            logging.debug(e)
+            await asyncio.sleep(1)  # sleep one second
+
+        except Exception as e:
+            await msg.reply(
+                "Traceback: <code>%s</code>\n\nServer Response: <code>%s</code>"
+                % (e, add_["server_resp"]),
             )
 
-            await msg.reply(text_, reply_markup=link)
-        
-    except Exception as e:
-        await msg.reply("Traceback: %s\n\nServer Response: %s" % (e, add_["server_resp"]))
+        else:break
 
 
 @dp.message_handler(commands="start")
