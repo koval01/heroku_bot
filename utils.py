a0efec98372f0ccaae9525b4cbb5256992de3318
Koval Yaroslav <koval@q-writer.com>
2021-09-07 14:23:51 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,8 +10,8 @@ async def porfirevich(user_text: str) -> str:
     }
     json = {
         "prompt": user_text,
-        "length": randint(30, 60),
-        "num_samples": 5
+        "length": randint(50, 60),
+        "num_samples": 1
     }
     url = "https://pelevin.gpt.dobro.ai/generate/"
 
